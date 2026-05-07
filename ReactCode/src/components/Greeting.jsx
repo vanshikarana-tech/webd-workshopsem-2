05ae0e0927f22803d17da6ab4784d12dbdfee99a
vanshikarana-tech <ranavansh008@gmail.com>
2026-05-07 15:14:18 +0530

    experiment

---
+++ ReactCode/src/components/Greeting.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Greeting(props) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <br/>
+      <p>{age}</p>
+    </div>
+    
+  )
+}
+
+export default Greeting
